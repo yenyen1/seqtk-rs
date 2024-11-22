0e048ac4339ce174e160ff5d73762af15ef61b32
Yen Yen Wang <wangyenyen.st00g@g2.nctu.edu.tw>
2024-11-22 16:35:23 -0500

    parallel array

--- src/fq_check.rs
+++ src/fq_check.rs
@@ -4,6 +4,7 @@ use crate::fx_iterator::FxIterator;
 use crate::qual_map::QualMap;
 use crate::sequence_read::SequenceRead;
 use crate::stats;
+use ndarray::{Array2, Axis};
 use rayon::prelude::*;
 
 // use std::fmt::{Debug, Display};
@@ -47,16 +48,16 @@ fn process_fastq_and_check(
     max_length: usize,
     qual_set: HashMap<u8, usize>,
 ) -> io::Result<()> {
-    let mut dna_count_mat = QualMap::init_dna_count_mat(max_length);
-    let mut qual_sum_map: QualMap = QualMap::init_qual_sum_mat(max_length);
+    let mut dna_count_mat = init_dna_count_mat(max_length);
+    let mut qual_sum_map = init_qual_sum_mat(max_length);
     // let mut qual_count_map = QualMap::init_qual_count_map(qual_threshold);
 
     let fq_iter = FxIterator::new(fq_path, "fastq").unwrap();
     for fx_lines in fq_iter {
         let cur_read = create_sequenceread(fx_lines, ascii_bases)?;
 
-        QualMap::update_map_with_read(&mut dna_count_mat, &cur_read, qual_threshold); // qual_value=0: count dna without threshold
-        QualMap::update_map_with_read(&mut qual_sum_map, &cur_read, qual_threshold);
+        update_dna_count(&mut dna_count_mat, &cur_read); // qual_value=0: count dna without threshold
+        update_qual_sum(&mut qual_sum_map, &cur_read);
         // QualMap::update_map_with_read(&mut qual_count_map, &cur_read, qual_threshold);
     }
 
@@ -83,19 +84,19 @@ fn create_sequenceread(fx_lines: Vec<String>, ascii_bases: u8) -> Result<Sequenc
 /// [[[ Write stats files ]]]
 fn write_fq_stat_to_file(
     path: &str,
-    dna_count_mat: &QualMap,
+    dna_count_mat: &Array2<f64>,
     // qual_count_map: &QualMap,
-    qual_sum_map: &QualMap,
+    qual_sum_map: &Array2<f64>,
     max_length: usize,
 ) -> io::Result<()> {
     let mut writer = append_bufwriter(path)?;
-    let (total, output_dna_stats) = QualMap::get_total_and_dna_proportion(dna_count_mat);
+    let (total, dna_count_str) = get_total_and_dna_proportion(dna_count_mat);
+    let qual_sum_str = get_total_qual_sum_stats(qual_sum_map, total);
+    writeln!(writer, "{}{}", dna_count_str, qual_sum_str)?;
     for pos in 0..max_length {
-        writeln!(
-            writer,
-            "{}",
-            QualMap::get_output(dna_count_mat, qual_sum_map, pos) // get_dna_stats_string(dna_count_map, qual_sum_map, qual_count_map, pos)
-        )?;
+        let (total, dna_count_str) = get_pos_total_and_dna_proportion(dna_count_mat, pos);
+        let qual_sum_str = get_qual_sum_stats(qual_sum_map, pos, total);
+        writeln!(writer, "{}{}", dna_count_str, qual_sum_str)?;
     }
     Ok(())
 }
@@ -197,4 +198,80 @@ fn parse_fq_and_write_length_and_qual_stats(
     write!(&mut buf_writer, "{}", out_string)?;
 
     Ok((*reads_size.last().unwrap() as usize, qual_idx_map))
+}
+
+/// Initial DNA count matrix array2[pos][dna] = count
+fn init_dna_count_mat(max_read_length: usize) -> Array2<f64> {
+    Array2::<f64>::zeros((max_read_length, 5))
+}
+/// Initial Qual sum mat array2[Pos][P/Q] = sum of Qual (P=0 ; Q=1)
+fn init_qual_sum_mat(max_read_length: usize) -> Array2<f64> {
+    Array2::<f64>::zeros((max_read_length, 2))
+}
+fn update_dna_count(dna_count_mat: &mut Array2<f64>, read: &SequenceRead) {
+    for (pos, &idx) in read.get_seq_idx().as_slice().iter().enumerate() {
+        if let Some(value) = dna_count_mat.get_mut((pos, idx)) {
+            *value += 1.0;
+        }
+    }
+}
+fn update_qual_sum(qual_mat: &mut Array2<f64>, read: &SequenceRead) {
+    fn add(
+        qual_mat: &mut Array2<f64>,
+        qual_vec: &[f64],
+        type_idx: usize, // P=0; Q=1
+    ) {
+        for (i, &v) in qual_vec.iter().enumerate() {
+            if let Some(value) = qual_mat.get_mut((i, type_idx)) {
+                *value += v;
+            }
+        }
+    }
+    let p_err_vec = read.get_p_err_vec(); // P_error
+    add(qual_mat, &p_err_vec, 0);
+    let q_score_vec = read.get_q_score_vec_f64(); // Q score
+    add(qual_mat, &q_score_vec, 1);
+}
+/// Get Total\t{#Base}\t{%A}\t{%C}\t{%G}\t{%T}\t{%N} String from dna_count_mat
+fn get_total_and_dna_proportion(dna_count_mat: &Array2<f64>) -> (f64, String) {
+    let total_dna_counts: Vec<f64> = (dna_count_mat)
+        .axis_iter(Axis(1)) // Sum across rows
+        .into_par_iter()
+        .map(|col| col.sum())
+        .collect();
+    let total: f64 = total_dna_counts.iter().sum();
+    let prop_str: String = total_dna_counts
+        .iter()
+        .map(|&c| format!("\t{:.1}", 100.0 * c / total))
+        .collect();
+
+    (total as f64, format!("Total\t{:.0}{}", total, prop_str))
+}
+/// Get {Pos}\t{#Base}\t{%A}\t{%C}\t{%G}\t{%T}\t{%N} String from dna_count_mat
+fn get_pos_total_and_dna_proportion(dna_count_mat: &Array2<f64>, upos: usize) -> (f64, String) {
+    let pos_total: f64 = dna_count_mat.slice(ndarray::s![upos, ..]).iter().sum(); // Sum of counts at position
+    let prop_str: String = dna_count_mat
+        .slice(ndarray::s![upos, ..])
+        .iter()
+        .map(|&c| format!("\t{:.1}", 100.0 * c as f64 / pos_total))
+        .collect();
+    (
+        pos_total,
+        format!("{}\t{}{}", upos + 1, pos_total, prop_str),
+    )
+}
+/// Get {avgQ}\t{Qerr} String from qual_sum_map
+fn get_qual_sum_stats(qual_sum_mat: &Array2<f64>, upos: usize, total: f64) -> String {
+    let q_avg = qual_sum_mat.get((upos, 1)).unwrap_or(&0.0) / total;
+    let p_avg = qual_sum_mat.get((upos, 0)).unwrap_or(&0.0) / total;
+    let q_err = SequenceRead::convert_p_err_to_q_score(p_avg);
+    format!("\t{:.1}\t{:.1}", q_avg, q_err)
+}
+/// Get {avgQ}\t{Qerr} String from qual_sum_map
+fn get_total_qual_sum_stats(qual_sum_mat: &Array2<f64>, total: f64) -> String {
+    let total_qual_sum = qual_sum_mat.sum_axis(Axis(0)); // Sum of Q/P f across rows
+    let q_avg = total_qual_sum.get(1).unwrap_or(&0.0) / total;
+    let p_avg = total_qual_sum.get(0).unwrap_or(&0.0) / total;
+    let q_err: f64 = SequenceRead::convert_p_err_to_q_score(p_avg);
+    format!("\t{:.1}\t{:.1}", q_avg, q_err)
 }
--- src/qual_map.rs
+++ src/qual_map.rs
@@ -10,25 +10,11 @@ pub enum QualMap {
     /// [ Qual count map ]
     /// If qual threshold is zero, print out the number of qual count for each position.
     /// If qual threshold is not zero, print out the % of low/high qual for each position.
-    DNACount(Array2<u32>), // array2[pos][dna] = count
-    QualCount(Array2<u32>),    // array2[pos][qual] = count
+    QualCount(Array2<u32>), // array2[pos][qual] = count
     LowQualCount(Array1<u32>), // array1[pos] = low_qual_count
-
-    /// [ Low qual sum ]
-    /// Calc avgQ and errQ for each position.
-    QualSum(Array2<f64>), // array2[Pos][P/Q] = sum of Qual (P=0 ; Q=1)
 }
 
 impl QualMap {
-    /// [Initial fqchk map]
-    /// Initial DNA count matrix array2[pos][dna] = count
-    pub fn init_dna_count_mat(max_read_length: usize) -> QualMap {
-        QualMap::DNACount(Array2::<u32>::zeros((max_read_length, 5)))
-    }
-    /// Initial Qual sum map {ScoreType: {Pos: sum of Qual}}
-    pub fn init_qual_sum_mat(max_read_length: usize) -> QualMap {
-        QualMap::QualSum(Array2::<f64>::zeros((max_read_length, 2)))
-    }
     /// Initial QualMap depend on the qual threshold.
     pub fn init_qual_count_map(
         max_read_length: usize,
@@ -57,12 +43,6 @@ impl QualMap {
     /// Update data with a sequence read
     pub fn update_map_with_read(qual_mat: &mut QualMap, read: &SequenceRead, qual_threshold: u8) {
         match qual_mat {
-            QualMap::DNACount(mat) => {
-                QualMap::update_dna_count(mat, read);
-            }
-            QualMap::QualSum(mat) => {
-                QualMap::update_qual_sum(mat, read);
-            }
             QualMap::QualCount(mat) => {
                 QualMap::update_qual_count(mat, read);
             }
@@ -71,31 +51,7 @@ impl QualMap {
             }
         }
     }
-    fn update_dna_count(dna_count_mat: &mut Array2<u32>, read: &SequenceRead) {
-        for (pos, &idx) in read.get_seq_idx().as_slice().iter().enumerate() {
-            if let Some(value) = dna_count_mat.get_mut((pos, idx)) {
-                *value += 1;
-            }
-            // dna_count_mat[(pos,idx)] += 1;
-        }
-    }
-    fn update_qual_sum(qual_map: &mut Array2<f64>, read: &SequenceRead) {
-        fn add(
-            qual_map: &mut Array2<f64>,
-            qual_vec: &[f64],
-            type_idx: usize, // P=0; Q=1
-        ) {
-            for (i, &v) in qual_vec.iter().enumerate() {
-                if let Some(value) = qual_map.get_mut((type_idx, i)) {
-                    *value += v;
-                }
-            }
-        }
-        let p_err_vec = read.get_p_err_vec(); // P_error
-        add(qual_map, &p_err_vec, 0);
-        let q_score_vec = read.get_q_score_vec_f64(); // Q score
-        add(qual_map, &q_score_vec, 1);
-    }
+
     fn update_low_qual_count(qual_mat: &mut Array1<u32>, read: &SequenceRead, qual_threshold: u8) {
         for (i, &q) in read.get_q_score_vec().as_slice().iter().enumerate() {
             if q < qual_threshold {
@@ -110,22 +66,6 @@ impl QualMap {
     }
 
     /// [Stats output]
-    pub fn get_output(
-        dna_count_map: &QualMap,
-        qual_sum_map: &QualMap,
-        // qual_count_map: &QualMap,
-        pos: usize,
-    ) -> String {
-        let (pos_total, output_dna_stats) =
-            QualMap::get_pos_total_and_dna_proportion(dna_count_map, pos);
-        let qual_sum_stats = QualMap::get_qual_sum_stats(qual_sum_map, pos, pos_total);
-        // let qual_count_stats = QualMap::get_qual_count_stats(qual_count_map,pos, pos_total);
-        let mut out = String::new();
-        out.push_str(&output_dna_stats);
-        out.push_str(&qual_sum_stats);
-        // out.push_str(&qual_count_stats);
-        out
-    }
     pub fn get_qual_count_stats(qual_map: &QualMap, pos: usize, total: f64) -> String {
         match qual_map {
             QualMap::LowQualCount(map) => QualMap::get_low_qual_stats(map, pos, total),
@@ -138,51 +78,5 @@ impl QualMap {
         let low_qual_total = low_qual_count_mat[pos];
         let p_low = 100.0 * low_qual_total as f64 / total;
         format!("\t{:.1}\t{:.1}", p_low, 100.0 - p_low)
-    }
-    /// Get Total\t{#Base}\t{%A}\t{%C}\t{%G}\t{%T}\t{%N} String from dna_count_mat
-    pub fn get_total_and_dna_proportion(dna_count_mat: &QualMap) -> (f64, String) {
-        match dna_count_mat {
-            QualMap::DNACount(mat) => {
-                let total_dna_counts = mat.sum_axis(ndarray::Axis(0)); // Sum of count for A/C/G/T/N (across rows/pos)
-                let total = total_dna_counts.sum() as f64;
-                let prop_str: String = total_dna_counts
-                    .iter()
-                    .map(|&c| format!("\t{:.1}", 100.0 * c as f64 / total))
-                    .collect();
-                (total, format!("Total\t{:.0}{}", total, prop_str))
-            }
-            _ => panic!("1 should not happen"),
-        }
-    }
-    /// Get {Pos}\t{#Base}\t{%A}\t{%C}\t{%G}\t{%T}\t{%N} String from dna_count_mat
-    pub fn get_pos_total_and_dna_proportion(dna_count_mat: &QualMap, pos: usize) -> (f64, String) {
-        match dna_count_mat {
-            QualMap::DNACount(mat) => {
-                let pos_total: u32 = mat.slice(ndarray::s![pos, ..]).iter().sum(); // Sum of counts at position
-                let total_f64: f64 = pos_total as f64;
-                let prop_str: String = mat
-                    .slice(ndarray::s![pos, ..])
-                    .iter()
-                    .map(|&c| format!("\t{:.1}", 100.0 * c as f64 / total_f64))
-                    .collect();
-                (total_f64, format!("{}\t{}{}", pos + 1, pos_total, prop_str))
-            }
-            _ => panic!("2 should not happen"),
-        }
-    }
-    /// Get {avgQ}\t{Qerr} String from qual_sum_map
-    pub fn get_qual_sum_stats(qual_sum_map: &QualMap, pos: usize, total: f64) -> String {
-        match qual_sum_map {
-            QualMap::QualSum(mat) => {
-                // P=0; Q=1
-                let q_avg = mat.get((pos, 1)).unwrap_or(&0.0) / total;
-                let p_avg = mat.get((pos, 0)).unwrap_or(&0.0) / total;
-                let q_err = SequenceRead::convert_p_err_to_q_score(p_avg);
-                format!("\t{:.1}\t{:.1}", q_avg, q_err)
-            }
-            _ => {
-                panic!("3 should not happen")
-            }
-        }
     }
 }
