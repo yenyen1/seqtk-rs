2715955fee089c4a1297a10cbea4168240759ffd
Yen Yen Wang <wangyenyen.st00g@g2.nctu.edu.tw>
2025-06-23 16:46:11 -0400

    add size.rs

---
+++ src/io_utils.rs
@@ -0,0 +1,88 @@
+use bio::io::{fasta, fastq};
+use flate2::read::GzDecoder;
+use std::fmt::Display;
+use std::fs::{File, OpenOptions};
+use std::io::{self, BufRead, BufReader, BufWriter, Stdout, Write};
+
+pub fn buffer_reader_maybe_gz(path: &str) -> io::Result<Box<dyn BufRead>> {
+    let file = File::open(path)?;
+    if path.ends_with(".gz") {
+        let gz_decoder = GzDecoder::new(file);
+        Ok(Box::new(BufReader::new(gz_decoder)))
+    } else {
+        Ok(Box::new(BufReader::new(file)))
+    }
+}
+pub fn append_bufwriter(out_path: &str) -> io::Result<BufWriter<File>> {
+    let file = OpenOptions::new()
+        .create(true) // Create the file if it doesn't exist
+        .append(true) // Open the file in append mode (no truncation)
+        .open(out_path)?; // Open the file at the provided path
+    let writer = BufWriter::new(file);
+    Ok(writer)
+}
+
+pub struct FaReader(fasta::Reader<BufReader<Box<dyn BufRead>>>);
+pub struct FqReader(fastq::Reader<BufReader<Box<dyn BufRead>>>);
+impl FaReader {
+    pub fn new(path: &str) -> io::Result<Self> {
+        let reader: Box<dyn BufRead> = buffer_reader_maybe_gz(path)?;
+        Ok(Self(fasta::Reader::new(reader)))
+    }
+    pub fn records(self) -> fasta::Records<BufReader<Box<dyn BufRead + 'static>>> {
+        self.0.records()
+    }
+}
+impl FqReader {
+    pub fn new(path: &str) -> io::Result<Self> {
+        let reader: Box<dyn BufRead> = buffer_reader_maybe_gz(path)?;
+        Ok(Self(fastq::Reader::new(reader)))
+    }
+    pub fn records(self) -> fastq::Records<BufReader<Box<dyn BufRead + 'static>>> {
+        self.0.records()
+    }
+}
+
+pub struct Output<W: Write> {
+    writer: W,
+}
+impl<W: Write> Output<W> {
+    pub fn new(writer: W) -> Self {
+        Output { writer }
+    }
+    pub fn write<T: Display>(&mut self, result: T) -> io::Result<()> {
+        write!(self.writer, "{}", result)
+    }
+}
+
+pub enum FxWriter {
+    Fasta(fasta::Writer<Stdout>),
+    Fastq(fastq::Writer<Stdout>),
+}
+impl FxWriter {
+    pub fn new(is_fasta: bool) -> FxWriter {
+        let std_out = io::stdout();
+        if is_fasta {
+            FxWriter::Fasta(fasta::Writer::new(std_out))
+        } else {
+            FxWriter::Fastq(fastq::Writer::new(std_out))
+        }
+    }
+    pub fn write(
+        &mut self,
+        id: &str,
+        seq: &[u8],
+        desc: Option<&str>,
+        qual: &[u8],
+    ) -> io::Result<()> {
+        match self {
+            FxWriter::Fasta(ref mut out) => {
+                out.write(id, desc, seq)?;
+            }
+            FxWriter::Fastq(ref mut out) => {
+                out.write(id, desc, seq, qual)?;
+            }
+        }
+        Ok(())
+    }
+}
---
+++ src/size.rs
@@ -0,0 +1,32 @@
+use crate::io_utils::{FaReader, FqReader, Output};
+use std::io;
+
+pub fn calc_fq_size(path: &str) -> Result<(), std::io::Error> {
+    let fq_iter = FqReader::new(path)?;
+    let mut seq_count = 0;
+    let mut bases_count = 0;
+    for record in fq_iter.records() {
+        let read = record.unwrap();
+        seq_count += 1;
+        bases_count += read.seq().len();
+    }
+    let stdout = io::stdout();
+    let mut output = Output::new(stdout);
+    output.write(format!("{} {}\n", seq_count, bases_count))?;
+    Ok(())
+}
+
+pub fn calc_fa_size(path: &str) -> Result<(), std::io::Error> {
+    let fa_iter = FaReader::new(path)?;
+    let mut seq_count = 0;
+    let mut bases_count = 0;
+    for record in fa_iter.records() {
+        let read = record.unwrap();
+        seq_count += 1;
+        bases_count += read.seq().len();
+    }
+    let stdout = io::stdout();
+    let mut output = Output::new(stdout);
+    output.write(format!("{} {}\n", seq_count, bases_count))?;
+    Ok(())
+}
