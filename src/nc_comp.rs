32e7f3a2d49e6a020d27174c6e289abf0a3b2169
Yen Yen Wang <wangyenyen.st00g@g2.nctu.edu.tw>
2025-06-27 15:50:13 -0400

    some update

--- src/nc_comp.rs
+++ src/nc_comp.rs
@@ -1,28 +1,32 @@
 use crate::bed::BedMap;
 use crate::dna::SeqComp;
 use crate::io_utils::{FaReader, FqReader, Output};
+use crate::record::RecordType;
 use std::io::{self, Stdout};
 
 pub fn calc_fq_comp_wo_bed(path: &str, exclude_masked: bool) -> Result<(), std::io::Error> {
     let fq_iter = FqReader::new(path)?;
     let stdout = io::stdout();
     let mut output = Output::new(stdout);
-    print_cols(&mut output)?;
     if exclude_masked {
         for record in fq_iter.records() {
-            let read = record.unwrap();
-            let mut count: [usize; 23] = [0; 23];
-            SeqComp::count_unmasked_nc(&mut count, read.seq(), 0, read.seq().len());
-            let result = SeqComp::get_unmasked_result(&count);
-            print(&mut output, read.id(), read.seq().len(), &result)?;
+            match record {
+                Ok(read) => {
+                    let result = cal_unmasked_seq(&read);
+                    print(&mut output, read.id(), read.seq().len(), &result)?;
+                }
+                Err(e) => eprintln!("Error read fASTQ: {}", e),
+            }
         }
     } else {
         for record in fq_iter.records() {
-            let read = record.unwrap();
-            let mut count: [usize; 23] = [0; 23];
-            SeqComp::count_all_nc(&mut count, read.seq(), 0, read.seq().len());
-            let result = SeqComp::get_all_result(&count);
-            print(&mut output, read.id(), read.seq().len(), &result)?;
+            match record {
+                Ok(read) => {
+                    let result = cal_all_seq(&read);
+                    print(&mut output, read.id(), read.seq().len(), &result)?;
+                }
+                Err(e) => eprintln!("Error read fASTQ: {}", e),
+            }
         }
     }
     Ok(())
@@ -31,22 +35,25 @@ pub fn calc_fa_comp_wo_bed(path: &str, exclude_masked: bool) -> Result<(), std::
     let fa_iter = FaReader::new(path)?;
     let stdout = io::stdout();
     let mut output = Output::new(stdout);
-    print_cols(&mut output)?;
     if exclude_masked {
         for record in fa_iter.records() {
-            let read = record.unwrap();
-            let mut count: [usize; 23] = [0; 23];
-            SeqComp::count_unmasked_nc(&mut count, read.seq(), 0, read.seq().len());
-            let result = SeqComp::get_unmasked_result(&count);
-            print(&mut output, read.id(), read.seq().len(), &result)?;
+            match record {
+                Ok(read) => {
+                    let result = cal_unmasked_seq(&read);
+                    print(&mut output, read.id(), read.seq().len(), &result)?;
+                }
+                Err(e) => eprintln!("Error read fASTA: {}", e),
+            }
         }
     } else {
         for record in fa_iter.records() {
-            let read = record.unwrap();
-            let mut count: [usize; 23] = [0; 23];
-            SeqComp::count_all_nc(&mut count, read.seq(), 0, read.seq().len());
-            let result = SeqComp::get_all_result(&count);
-            print(&mut output, read.id(), read.seq().len(), &result)?;
+            match record {
+                Ok(read) => {
+                    let result = cal_all_seq(&read);
+                    print(&mut output, read.id(), read.seq().len(), &result)?;
+                }
+                Err(e) => eprintln!("Error read fASTA: {}", e),
+            }
         }
     }
     Ok(())
@@ -60,31 +67,28 @@ pub fn calc_fq_comp_with_bed(
     let fq_iter = FqReader::new(path)?;
     let stdout = io::stdout();
     let mut output = Output::new(stdout);
-    print_cols(&mut output)?;
     if exclude_masked {
         let bedmap = BedMap::from(bed)?;
         for record in fq_iter.records() {
-            let read = record.unwrap();
-            let mut count: [usize; 23] = [0; 23];
-            if let Some(bedvec) = bedmap.get(read.id()) {
-                bedvec.iter().for_each(|pos| {
-                    SeqComp::count_unmasked_nc(&mut count, read.seq(), pos.0, pos.1);
-                });
-                let result = SeqComp::get_unmasked_result(&count);
-                print(&mut output, read.id(), read.seq().len(), &result)?;
+            match record {
+                Ok(read) => {
+                    if let Some(result) = cal_unmasked_seq_with_bed(&read, &bedmap) {
+                        print(&mut output, read.id(), read.seq().len(), &result)?;
+                    }
+                }
+                Err(e) => eprintln!("Error read fASTQ: {}", e),
             }
         }
     } else {
         let bedmap = BedMap::from(bed)?;
         for record in fq_iter.records() {
-            let read = record.unwrap();
-            let mut count: [usize; 23] = [0; 23];
-            if let Some(bedvec) = bedmap.get(read.id()) {
-                bedvec.iter().for_each(|pos| {
-                    SeqComp::count_all_nc(&mut count, read.seq(), pos.0, pos.1);
-                });
-                let result = SeqComp::get_all_result(&count);
-                print(&mut output, read.id(), read.seq().len(), &result)?;
+            match record {
+                Ok(read) => {
+                    if let Some(result) = cal_all_seq_with_bed(&read, &bedmap) {
+                        print(&mut output, read.id(), read.seq().len(), &result)?;
+                    }
+                }
+                Err(e) => eprintln!("Error read fASTQ: {}", e),
             }
         }
     }
@@ -99,40 +103,64 @@ pub fn calc_fa_comp_with_bed(
     let fa_iter = FaReader::new(path)?;
     let stdout = io::stdout();
     let mut output = Output::new(stdout);
-    print_cols(&mut output)?;
     if exclude_masked {
         let bedmap = BedMap::from(bed)?;
         for record in fa_iter.records() {
-            let read = record.unwrap();
-            let mut count: [usize; 23] = [0; 23];
-            if let Some(bedvec) = bedmap.get(read.id()) {
-                bedvec.iter().for_each(|pos| {
-                    SeqComp::count_unmasked_nc(&mut count, read.seq(), pos.0, pos.1);
-                });
-                let result = SeqComp::get_unmasked_result(&count);
-                print(&mut output, read.id(), read.seq().len(), &result)?;
+            match record {
+                Ok(read) => {
+                    if let Some(result) = cal_unmasked_seq_with_bed(&read, &bedmap) {
+                        print(&mut output, read.id(), read.seq().len(), &result)?;
+                    }
+                }
+                Err(e) => eprintln!("Error read fASTA: {}", e),
             }
         }
     } else {
         let bedmap = BedMap::from(bed)?;
         for record in fa_iter.records() {
-            let read = record.unwrap();
-            let mut count: [usize; 23] = [0; 23];
-            if let Some(bedvec) = bedmap.get(read.id()) {
-                bedvec.iter().for_each(|pos| {
-                    SeqComp::count_all_nc(&mut count, read.seq(), pos.0, pos.1);
-                });
-                let result = SeqComp::get_all_result(&count);
-                print(&mut output, read.id(), read.seq().len(), &result)?;
+            match record {
+                Ok(read) => {
+                    if let Some(result) = cal_all_seq_with_bed(&read, &bedmap) {
+                        print(&mut output, read.id(), read.seq().len(), &result)?;
+                    }
+                }
+                Err(e) => eprintln!("Error read fASTA: {}", e),
             }
         }
     }
     Ok(())
 }
-
-fn print_cols(output: &mut Output<Stdout>) -> Result<(), std::io::Error> {
-    output.write("ID\tlength\t#A\t#C\t#G\t#T\t#2\t#3\t#4\t#CG\t#GC\n")?;
-    Ok(())
+fn cal_all_seq<T:RecordType>(read: &T) -> [usize; 9] {
+    let mut count: [usize; 23] = [0; 23];
+    SeqComp::count_all_nc(&mut count, read.seq(), 0, read.seq().len());
+    SeqComp::get_all_result(&count)
+}
+fn cal_unmasked_seq<T:RecordType>(read: &T) -> [usize; 9] {
+    let mut count: [usize; 23] = [0; 23];
+    SeqComp::count_unmasked_nc(&mut count, read.seq(), 0, read.seq().len());
+    SeqComp::get_unmasked_result(&count)
+}
+fn cal_all_seq_with_bed<T: RecordType>(read: &T, bedmap: &BedMap) -> Option<[usize; 9]> {
+    let mut count: [usize; 23] = [0; 23];
+    if let Some(bedvec) = bedmap.get(read.id()) {
+        bedvec.iter().for_each(|pos| {
+            SeqComp::count_all_nc(&mut count, read.seq(), pos.0, pos.1);
+        });
+        let result = SeqComp::get_all_result(&count);
+        return Some(result)
+    }
+    None
+}
+fn cal_unmasked_seq_with_bed<T: RecordType>(read: &T, bedmap: &BedMap) -> Option<[usize; 9]> {
+    let mut count: [usize; 23] = [0; 23];
+    if let Some(bedvec) = bedmap.get(read.id()) {
+        bedvec.iter().for_each(|pos| {
+            SeqComp::count_unmasked_nc(&mut count, read.seq(), pos.0, pos.1);
+        });
+        let result = SeqComp::get_unmasked_result(&count);
+        return Some(result)
+    }
+    None
 }
 fn print(
     output: &mut Output<Stdout>,
